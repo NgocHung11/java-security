161994e4996d176af5432d1e0b68acdad6ee9566
NgocHung11 <22654091.hung@student.iuh.edu.vn>
2025-11-02 20:15:15 +0700

    add project

---
+++ Prooject-management/src/test/java/me/pnh/Prooject_management/ProojectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pnh.Prooject_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProojectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
